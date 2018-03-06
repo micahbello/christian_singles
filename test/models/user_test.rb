2e65e3b434843b5c5166915efc1836c462d451d4
Micah <micahbello@gmail.com>
2018-03-06 15:51:14 -0500

    User model and controller

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  session_token   :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
