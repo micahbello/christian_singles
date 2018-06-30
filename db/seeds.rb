ec905cdba4dea85626f7fecbb0247782c0ea5b49
Micah <micahbello@gmail.com>
2018-06-30 10:14:54 -0400

    Populate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,58 +6,88 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(username: "john342", password: "password", first_name: "John",
+user1 = User.create(username: "john342", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11772,
   age: 22, last_online: "2018-03-20", sex_seek: "Women",  min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "http://www.1920s-fashion-and-music.com/images/x1920s-mens-fashion.jpg.pagespeed.ic.gYJSob2E2l.jpg")
 
-user1 = User.create(username: "mely92", password: "password", first_name: "John",
+user1 = User.create(username: "mely92", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "female", birth_date: "1992-08-09", zip_code: 11772,
   age: 23, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "https://i.pinimg.com/originals/31/6c/e0/316ce04e3c0aea8299fefade7a696307.jpg")
 
-user1 = User.create(username: "kennydaguy2", password: "password", first_name: "John",
+user1 = User.create(username: "kennydaguy2", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11385,
   age: 22, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "http://www.mens-hairstyle.com/wp-content/uploads/2017/09/Bearded-Guy.jpg")
 
-user1 = User.create(username: "kevinbello", password: "password", first_name: "John",
+user1 = User.create(username: "kevinbello", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1989-08-09", zip_code: 08647,
   age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "http://atlantablackstar.com/wp-content/uploads/2015/09/michael-b-jordan-gq.jpg")
 
-user1 = User.create(username: "karla000", password: "password", first_name: "John",
+user1 = User.create(username: "karla000", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "female", birth_date: "1991-08-09", zip_code: 33101,
   age: 21, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "https://i.pinimg.com/originals/b4/2c/2e/b42c2ef3c947a5af03fc8699ccc1a028.jpg")
 
-user1 = User.create(username: "lovelygurl", password: "password", first_name: "John",
+user1 = User.create(username: "lovelygurl", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "female", birth_date: "1967-08-09", zip_code: 60131,
   age: 50, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
   image: "https://48palw1jqfwf1zkjitvyccc1-wpengine.netdna-ssl.com/wp-content/uploads/2016/10/IMG_4329.jpg")
 
-user1 = User.create(username: "heyrey", password: "password", first_name: "John",
+user1 = User.create(username: "heyrey", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1985-08-09", zip_code: 11772,
   age: 33, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "https://img.huffingtonpost.com/asset/587d498a1700002e00fdef27.jpeg?ops=scalefit_600_noupscale")
 
-user1 = User.create(username: "ma_nishma99", password: "password", first_name: "John",
+user1 = User.create(username: "ma_nishma99", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1995-08-09", zip_code: 10012,
   age: 23, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "https://pbs.twimg.com/media/DLYkhgeXkAAqKLQ.jpg")
 
-user1 = User.create(username: "randomguy73", password: "password", first_name: "John",
+user1 = User.create(username: "randomguy73", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "male", birth_date: "1990-08-09", zip_code: 10012,
   age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
   image: "https://i.pinimg.com/originals/b3/e1/7b/b3e17b4fdcf0067ea0a79e4c5510ed20.jpg")
+
+user1 = User.create(username: "elephant", password: "bereshitbara", first_name: "John",
+  last_name: "Smith", gender: "male", birth_date: "1990-08-09", zip_code: 10012,
+  age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
+  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg")
+
+user1 = User.create(username: "natti", password: "bereshitbara", first_name: "John",
+  last_name: "Smith", gender: "female", birth_date: "1990-08-09", zip_code: 10012,
+  age: 28, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
+  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+  image: "https://i1.sndcdn.com/artworks-000299942178-705cuu-t500x500.jpg")
+
+user1 = User.create(username: "Geisha", password: "bereshitbara", first_name: "John",
+  last_name: "Smith", gender: "female", birth_date: "1990-08-09", zip_code: 10012,
+  age: 28, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
+  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+  image: "https://cdn7.japanrailpass.com.au/wp-content/uploads/2012/07/Portrait-of-a-Maiko-geisha-in-Gion-Kyoto.jpg")
+
+user1 = User.create(username: "tatted", password: "bereshitbara", first_name: "John",
+  last_name: "Smith", gender: "male", birth_date: "1990-08-09", zip_code: 10012,
+  age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
+  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+  image: "http://www.hypehair.com/wp-content/uploads/2014/12/hype-hair-soulja-boy.jpeg?x37582")
+
+user1 = User.create(username: "link", password: "bereshitbara", first_name: "John",
+  last_name: "Smith", gender: "male", birth_date: "1991-08-09", zip_code: 33101,
+  age: 21, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
+  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+  image: "http://t-higashi.fool.jp/breath-of-the-wild/wp-content/uploads/2017/03/00000286_10.jpg")
 
 # user1 = User.create(username: "demoaccount", password: "password", first_name: "John",
 #   last_name: "Smith", gender: "male", birth_date: "1989-08-09", zip_code: 10012,
