36f5e2fc7b19db03701bef7f3866d524fd1a9bb8
Micah <micahbello@gmail.com>
2018-06-30 10:59:00 -0400

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(username: "john342", password: "bereshitbara", first_name: "John",
-  last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11772,
-  age: 22, last_online: "2018-03-20", sex_seek: "Women",  min_age_seek: 18, max_age_seek: 49,
-  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
-  image: "http://www.1920s-fashion-and-music.com/images/x1920s-mens-fashion.jpg.pagespeed.ic.gYJSob2E2l.jpg")
+# user1 = User.create(username: "john342", password: "bereshitbara", first_name: "John",
+#   last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11772,
+#   age: 22, last_online: "2018-03-20", sex_seek: "Women",  min_age_seek: 18, max_age_seek: 49,
+#   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+#   image: "http://www.1920s-fashion-and-music.com/images/x1920s-mens-fashion.jpg.pagespeed.ic.gYJSob2E2l.jpg")
 
-user1 = User.create(username: "mely92", password: "bereshitbara", first_name: "John",
-  last_name: "Smith", gender: "female", birth_date: "1992-08-09", zip_code: 11772,
-  age: 23, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
-  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
-  image: "https://i.pinimg.com/originals/31/6c/e0/316ce04e3c0aea8299fefade7a696307.jpg")
+# user1 = User.create(username: "mely92", password: "bereshitbara", first_name: "John",
+#   last_name: "Smith", gender: "female", birth_date: "1992-08-09", zip_code: 11772,
+#   age: 23, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
+#   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+#   image: "https://i.pinimg.com/originals/31/6c/e0/316ce04e3c0aea8299fefade7a696307.jpg")
 
-user1 = User.create(username: "kennydaguy2", password: "bereshitbara", first_name: "John",
-  last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11385,
-  age: 22, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
-  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
-  image: "http://www.mens-hairstyle.com/wp-content/uploads/2017/09/Bearded-Guy.jpg")
+# user1 = User.create(username: "kennydaguy2", password: "bereshitbara", first_name: "John",
+#   last_name: "Smith", gender: "male", birth_date: "1993-08-09", zip_code: 11385,
+#   age: 22, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
+#   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+#   image: "http://www.mens-hairstyle.com/wp-content/uploads/2017/09/Bearded-Guy.jpg")
+#
+# user1 = User.create(username: "kevinbello", password: "bereshitbara", first_name: "John",
+#   last_name: "Smith", gender: "male", birth_date: "1989-08-09", zip_code: 11385,
+#   age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
+#   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+#   image: "http://atlantablackstar.com/wp-content/uploads/2015/09/michael-b-jordan-gq.jpg")
 
-user1 = User.create(username: "kevinbello", password: "bereshitbara", first_name: "John",
-  last_name: "Smith", gender: "male", birth_date: "1989-08-09", zip_code: 11385,
-  age: 28, last_online: "2018-03-20", sex_seek: "Women", min_age_seek: 18, max_age_seek: 49,
-  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
-  image: "http://atlantablackstar.com/wp-content/uploads/2015/09/michael-b-jordan-gq.jpg")
-
-user1 = User.create(username: "karla000", password: "bereshitbara", first_name: "John",
-  last_name: "Smith", gender: "female", birth_date: "1991-08-09", zip_code: 33101,
-  age: 21, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
-  min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
-  image: "https://i.pinimg.com/originals/b4/2c/2e/b42c2ef3c947a5af03fc8699ccc1a028.jpg")
+# user1 = User.create(username: "karla000", password: "bereshitbara", first_name: "John",
+#   last_name: "Smith", gender: "female", birth_date: "1991-08-09", zip_code: 33101,
+#   age: 21, last_online: "2018-03-20", sex_seek: "Men", min_age_seek: 18, max_age_seek: 49,
+#   min_height_seek: 48, max_height_seek: 96, distance_seek: 100,
+#   image: "https://i.pinimg.com/originals/b4/2c/2e/b42c2ef3c947a5af03fc8699ccc1a028.jpg")
 
 user1 = User.create(username: "lovelygurl", password: "bereshitbara", first_name: "John",
   last_name: "Smith", gender: "female", birth_date: "1967-08-09", zip_code: 60131,
