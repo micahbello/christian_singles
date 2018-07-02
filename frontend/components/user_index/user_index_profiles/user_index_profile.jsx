4f901fcd8b438c8f6bf600c0e8ea64a682ac96af
Micah <micahbello@gmail.com>
2018-07-02 17:03:14 -0400

    Make minor css fixes and correct content on index

--- frontend/components/user_index/user_index_profiles/user_index_profile.jsx
+++ frontend/components/user_index/user_index_profiles/user_index_profile.jsx
@@ -6,8 +6,11 @@ class UserIndexProfile extends React.Component {
     super(props);
   }
 
-  componentDidMount(){
+  inchesToFeet(inches) {
+    let feet = Math.floor(inches/12);
+    let remainingInches = inches % 12;
 
+    return `${feet}'${remainingInches}"`
   }
 
   //<b><font color="#00FF00">Online Now</font></b>
@@ -22,18 +25,25 @@ class UserIndexProfile extends React.Component {
 
         </div>
 
-          <span className="user-index-profile-name-text">
-            {this.props.profileDisplayName ? this.props.profileDisplayName : this.props.profileUsername}, {this.props.profileAge}
-            {this.props.profileOnlineStatus === true ? <div className="online-green-circle"></div> : ""}
-          </span>
+          <section>
+            <span className="user-index-profile-name-text">
+              {this.props.profileDisplayName ? this.props.profileDisplayName : this.props.profileUsername}, {this.props.profileAge}
+              {this.props.profileOnlineStatus === true ? <div className="online-green-circle"></div> : ""}
+            </span>
 
-          <span className="user-index-city-state-text">
-            {this.props.profileCity}, {this.props.profileState}
-          </span>
+            <span className="user-index-city-state-text">
+              {this.props.profileCity}, {this.props.profileState}
+            </span>
+          </section>
 
-          <span className="user-index-match">
-            {this.props.matchPercent}% Match
-          </span>
+          <section className="index-match-height">
+            <span className="user-index-match">
+              {this.props.matchPercent}% Match
+            </span>
+            <span className="user-index-height">
+              {this.props.profileHeight ? this.inchesToFeet(this.props.profileHeight) : ""}
+            </span>
+          </section>
 
           <span className="user-index-religion-occupation">
             {this.props.profileReligion ? this.props.profileReligion : ""}
