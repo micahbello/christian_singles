7877873ebb2e2fb1ddb1ae2c1e88dbe051c77e02
Micah <micahbello@gmail.com>
2018-09-25 16:34:19 -0400

    Make landing background video

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../footer/footer';
 import GuestLoginWindowContainer from '../guest_login_window/guest_login_container';
+import { Player } from 'video-react';
 
 class Welcome extends React.Component {
   constructor(props) {
@@ -19,32 +20,45 @@ class Welcome extends React.Component {
         { this.props.currentWindow === "GuestLoginWindow" ? <GuestLoginWindowContainer /> : null}
         { this.props.currentWindow != null ? document.getElementsByTagName("body")[0].style="overflow: hidden" : null}
 
-        <section className="member-login-link-section">
-          <span>Already a Member?</span>
-          <Link className="link-to-log-in" to='/login' >Login Here</Link>
-        </section>
 
-        <section className="text-box">
-          <img src={fish} alt="fish"/>
 
-          <h1>Christian Singles</h1>
-          <h2>1 Corinthians 13:4</h2>
+        <section className="landing-header  container">
 
-          <p>Love is patient. Love is kind. Come find this love.</p>
+          <div className="video-wrap">
+            <video id="background-video" loop autoPlay>
+              <source src="https://s3.us-east-2.amazonaws.com/christian-singles-pro/Aerial+Video+Of+Married+Couple.mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
 
-          <section className="sign-up-section">
+          <section className="landing-header-content">
+            <section className="member-login-link-section">
+              <span>Already a Member?</span>
+              <Link className="link-to-log-in" to='/login' >Login Here</Link>
+            </section>
 
-            <Link className="link-to-sign-up" to='/signup'>Create an account</Link>
+            <section className="text-box">
+              <img src={fish} alt="fish"/>
 
-            <button className= "guest-login"
-                onClick={() => this.props.updateUiWindow("GuestLoginWindow")}>
-                Guest Login
-            </button>
+              <h1>Christian Singles</h1>
+              <h2>1 Corinthians 13:4</h2>
 
+              <p>Love is patient. Love is kind. Come find this love.</p>
+
+              <section className="sign-up-section">
+
+                <Link className="link-to-sign-up" to='/signup'>Create an account</Link>
+
+                <button className= "guest-login"
+                    onClick={() => this.props.updateUiWindow("GuestLoginWindow")}>
+                    Guest Login
+                </button>
+
+              </section>
+
+            </section>
           </section>
-
         </section>
-
       </div>
     );
   }
